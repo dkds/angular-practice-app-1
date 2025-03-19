53c30b6d70a3a596f8290b5e374afb8113b6c2bb
dkds <dkds@users.noreply.github.com>
2025-03-19 20:50:51 +0530

    Add ngRx integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
-  ],
+    provideStore(reducers, { metaReducers })
+],
 };
