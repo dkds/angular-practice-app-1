4f8705e01adb12ff54925a0b15eef4142a5cdf47
dkds <dkds@users.noreply.github.com>
2025-03-22 20:26:29 +0530

    Add proper actions

--- src/app/store/task.actions.ts
+++ src/app/store/task.actions.ts
@@ -1,4 +1,5 @@
-import { Action, createAction } from '@ngrx/store';
+import { Action, createAction, props } from '@ngrx/store';
+import { Task } from '../models/task.model';
 
 export enum TaskActionType {
   LOAD_TASKS = '[Task] Load Tasks',
@@ -8,4 +9,23 @@ export enum TaskActionType {
   UPDATE_TASK_STATUS = '[Task] Update Task Status',
 }
 
-export const loadTasks = createAction(TaskActionType.LOAD_TASKS, );
+export const loadTasks = createAction(
+  TaskActionType.LOAD_TASKS,
+  props<{ query: string }>()
+);
+export const addTask = createAction(
+  TaskActionType.ADD_TASK,
+  props<{ task: Task }>()
+);
+export const updateTask = createAction(
+  TaskActionType.UPDATE_TASK,
+  props<{ task: Task }>()
+);
+export const deleteTask = createAction(
+  TaskActionType.DELETE_TASK,
+  props<{ id: number }>()
+);
+export const updateTaskStatus = createAction(
+  TaskActionType.UPDATE_TASK_STATUS,
+  props<{ id: number; completed: boolean }>()
+);
