04c21f9718bdd177f1088c10fb98c01473ba6ace
dkds <dkds@users.noreply.github.com>
2025-03-25 01:53:23 +0530

    Refactor registering of reducers as per recommended way in the docs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { reducers } from './store';
+import { taskReducer } from './store/task.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
-    provideStore(reducers),
+    provideStore(),
+    provideState({ name: 'task', reducer: taskReducer }),
   ],
 };
